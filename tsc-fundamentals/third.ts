abf1a71887d93aa9f2d740741f9f85fb67714fb9
peang <pngo93@gmail.com>
2024-02-15 19:00:42 -0600

    TS aliases and interfaces

--- tsc-fundamentals/third.ts
+++ tsc-fundamentals/third.ts
@@ -52,3 +52,62 @@ enum Directions {
 
 console.log(Directions.North); //logs North
 console.log(Directions.South); //logs South
+
+
+
+/* TS allows types to be defined separately from the variables that use them.
+   Aliases and Interfaces allows types to be easily shared bewteen different variables/objects  */
+
+//Type Aliases
+// - Allow defining types with a custom name (an Alias)
+// - can be used for primitives like string or more complex types such as objects and arrays:
+
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel
+}
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const car: Car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+}
+
+//INterfaces
+// - are similar to type aliases, except they only apply to object types.
+
+interface Rectangle {
+    height: number,
+    width: number
+}
+
+const rectangle: Rectangle = {
+    height: 20,
+    width: 10
+}
+
+//Extending Interfaces
+// - Interfaces can extend each other's definition
+// Extending an interface means you are creating a new interface with the same properties as the original, plus something new
+
+interface Square {
+    height: number,
+    width: number
+}
+
+interface ColoredSquare extends Rectangle {
+    color: string
+}
+
+const coloredSquare: ColoredSquare = {
+    height: 20,
+    width: 20,
+    color: "red"
+}
